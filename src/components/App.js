210dd08ab5bc8ffefad8eb210f08bb36a0951afb
Fun-25 <muskandodmani760@gmail.com>
2023-06-25 17:46:39 +0530

    submitting code-Sun Jun 25 2023 17:46:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
-
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+import React, { useState,useEffect } from "react";
+import "./../styles/App.css";
+import axios from "axios";
+ const App = () => {
+  const [searchTerm, setsearchTerm] = useState("");
+   const [movies,setMovies]=useState(undefined)
+   function handleSearchMovie(event) {
+    event.preventDefault();
+    axios
+      .get("https://www.omdbapi.com/", {
+        params: {
+          apikey: "b3f19237",
+          s: searchTerm,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.Search);
+        setsearchTerm("");
+      })
+      .catch((err) => setError("Invalid movie name. Please try again."));
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h2>Search Movie</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter Movie Name"
+          onChange={(e) => setsearchTerm(e.target.value)}
+          value={search}
+        />
+        <button type="submit" onClick={handleSearchMovie}>
+          Search
+        </button>
+      </form>
 
-export default App
+      <ul>
+        {movies != undefined ? (
+          movies.map((movie) => (
+            <div>
+              <li>
+                <h1>
+                  {movie.Title}
+                  {movie.Year}
+                </h1>
+                <img src={movie.Poster} alt="{movie.Title}" />
+              </li>
+            </div>
+          ))
+        ) : (
+          <p className="error">{error}</p>
+        )}
+      </ul>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
